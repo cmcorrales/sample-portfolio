80788f2b5dbfd52a46402891f91e5ec86940c9c8
Cassandra <cmcorrales3@gmail.com>
2018-04-06 08:23:30 -0400

    adjust file structure

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 $(function() {
 $('#projects').on('click', function(e){
     e.preventDefault();
-    document.body.style.backgroundImage='none';
     $('.main-content').remove();
     $('#ajax-container-about').empty();
     $('#ajax-container-projects').load("ajax/projects.html");
 })
 $('#about').on('click', function(e){
     e.preventDefault();
-    document.body.style.backgroundImage='none';
     $('.main-content').remove();
     $('#ajax-container-projects').empty();
     $('#ajax-container-about').load("ajax/about.html");
   })
-$()
+$('.icon').on('click', function(e){
+    e.preventDefault();
+    $('body').empty();
+    $('body').load("index.html");
+  })
 });
